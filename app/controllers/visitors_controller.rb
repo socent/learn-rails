48686c4262be13ac0f879046dc0c146e2ffbd8d9
socent <brad_wilcox@me.com>
2014-03-11 15:16:44 -0400

    Add navigation partials and Debugging errors

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,7 @@
 class VisitorsController < ApplicationController
 	def new
-    @owner = Owner.new
-  end
+	    Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + ': entering new method'
+	    @owner = Owner.new
+	    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+  	end
 end
